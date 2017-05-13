97d63c55b19d4c388c1f46e87ac52d1f76350d1f
betzaidatolayo <league@Mac-mini.local>
2017-05-13 11:26:38 -0700

    knock knock jokes nana nana who nana you business

---
+++ src/knockknock.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("knockknock");
+JOptionPane.showInputDialog("nana");
+JOptionPane.showInputDialog("nanayourbusiness");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
---
+++ src/morningZombie.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class morningZombie {
+public static void main(String[] args) {
+	System.out.println("hi,my name is juan");
+JOptionPane.showMessageDialog(null, "My name is juan");
+JOptionPane.showMessageDialog(null, "change your clothes");
+JOptionPane.showMessageDialog(null, "and eat some breakfast");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
