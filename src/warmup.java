de2d2f1af6c5c2e67a1ee014fedbfca8fec62a27
betzaidatolayo <league@WTS-MM-7.local>
2017-05-27 11:26:25 -0700

    betzaida learn to make riddles

---
+++ src/warmup.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hi my name is Betzaida");
+		JOptionPane.showMessageDialog(null, "I  like polar bears");
+		JOptionPane.showMessageDialog(null, "I like cartoon");
+		JOptionPane.showMessageDialog(null, "I love pizza");
+		JOptionPane.showMessageDialog(null, "I like cats");
+		JOptionPane.showMessageDialog(null, "And dogs");
+
+	}
+
+}
