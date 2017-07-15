a0be346170fdc0a83d499b5da945b4e20d2db16f
League <league@Mac-mini-4.local>
2017-07-15 11:35:30 -0700

    haha i hack you

---
+++ src/Adidas.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Adidas {
+public static void main(String[] args) {
+//JOptionPane.showMessageDialog(null,  "hi ");
+//String name=JOptionPane.showInputDialog("what your name");
+// String name2=JOptionPane.showInputDialog( "good name of your's"+name+"do you nlike polar if not goto canda");
+String favorite=JOptionPane.showInputDialog("what's your fav toys");
+JOptionPane.showMessageDialog(null, "they are to play with"+ favorite);
+
+
+
+
+
+
+
+}
+}
---
+++ src/Creeper.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Creeper {
+public static void main(String[] args) {
+	String pin = JOptionPane.showInputDialog("hi,my name is kathia mesa I'm from t moblie i was calling becacuse you haven't pay your finish bill. I was wondering if you can give me you pin for your finishing bill");	
+	JOptionPane.showMessageDialog(null, "HAHA you gave your pin to buy a iphone 7 plus in black "+ pin);
+}
+}
